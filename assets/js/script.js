544ee7a5510b0a540418ac4e3615fc1d99109443
Israel Oliveira <israeloliveira.dev@gmail.com>
2022-10-23 21:45:12 -0300

    Front contador

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+let contador = document.querySelector("#numCont");
+let interval = 5000;
+
+let startValue = 0;
+let endValue = parseInt(contador.getAttribute("data-val"));
+
+let duracao = Math.floor(interval/ endValue)
+
+let counter = setInterval(function(){
+  startValue += 1;
+  contador.textContent = startValue
+  if(startValue ==  endValue){
+    clearInterval(counter)
+  }
+})
+
+//Macbok
+
+const munitor = document.querySelector(".munitor")
+const linguagens = document.querySelector("#efeito")
+
+// munitor.addEventListener("click", ()=>{
+//   munitor.classList.toggle('baixado')
+// })
+
+
+setInterval(cagada, 13500)
+
+function cagada(){
+  setTimeout(munitor2, 2000);
+
+  setTimeout(aparecaLinguagem, 3300);
+  
+  setTimeout(aparecaLinguagem, 13000)
+  
+  setTimeout(munitor2, 13000);
+}
+
+
+
+
+function munitor2(){
+  munitor.classList.toggle('baixado')
+}
+
+function aparecaLinguagem(){
+  linguagens.classList.toggle('linguagens')
+  linguagens.classList.toggle('linguagens-hid')
+}
+
+
